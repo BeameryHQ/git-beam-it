c0b141dc5c98ad18d6b42eba8a4744e8ead9aa71
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2017-03-14 01:47:37 +0000

    first commit to push the code to github

---
+++ README.md
@@ -0,0 +1,58 @@
+# Beam-it: Bulk clone Github Repositories
+
+This bash script will bulk clone Github repositories for a specific user/organisation or team.
+
+The script will automatically configure itself by asking the user to enter some required information in order to be able to communicate with the Github API.
+
+> The script has no external dependencies except for [jq](https://stedolan.github.io/jq/) which is only required to parse the team list.
+
+![beam-it](https://dl.dropboxusercontent.com/u/5258344/Blog/git-beam-it%20public.gif)
+
+When running the script for the first time, the user will be prompted to enter his Github username and API personal access token. For more information on API personal access tokens, check this [Github tutorial](https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/)
+
+## Installation
+
+You can use the script `git-beam-it.sh` by exporting that into your .`bash_profile`, `.bashrc` or `.zshrc` and then make sure that it is executable with `chmod +x git-beam-it.sh`. However, the recommended way to use it is by registering it as a git plugin.
+
+The main requirement for git plugins is that the name of the shell script should be `git-name` where `name` is the command you want to run after typing git. Our script, `beam-it` is already configured accordingly.
+
+Now, you only need to put the script in `/usr/local/bin` or somewhere similar in your `$PATH` and thats it !
+
+![beam-it setup](https://dl.dropboxusercontent.com/u/5258344/Blog/git-beam-it%20setup.gif)
+
+> Note: After first setup, you might need to source your .bash_profile, .bashrc or .zshrc in order for the variables export to take effect and not to be prompted again
+
+```bash
+Usage: beamit <options>
+
+Options:
+-h        	   help            show help
+-i        	   interactive     interactive clone mode. The user will be prompted before cloning each repo
+-p             pulbic          clone only public repositories (note that this does not work for teams)
+-v        	   private         clone only private repositories (note that this does not work for teams)
+-r             regex           filter repositories based on this regex
+-t <teamId>    team            clone only repositories belonging to this specific team id
+-o <orgName>   organisation    clone only repositories belonging to this specific organisation name
+```
+
+If the paramteres `-t` and `-o` have been left empty, then the script will fetch the list of ogranisations and teams for that specific user, the user will then be prompted to enter the organisation name or team id or just skip to fetch all repositories.
+
+### Examples:
+
+```bash
+# Clone interactively all the private repositories for the user
+git beam-it -v -i
+
+# Clone interactively all the public repos that match the regex .*SeedJobs.* (any repo that contain SeedJobs)
+git beam-it -i -r .*SeedJobs.*
+
+# Clone all the public repositores for organisation SeedJobs
+git beam-it -p -o SeedJobs
+```
+![beam-it team](https://dl.dropboxusercontent.com/u/5258344/Blog/git-beam-it%20team.gif)
+
+> beam-it prompts the user to select from his list of teams if no team id was defined
+
+![beam-it regex](https://dl.dropboxusercontent.com/u/5258344/Blog/git-beam-it%20regex.gif)
+
+> beam-it can filter repos by a passed regex and will exclude those that do not match
---
+++ git-beam-it.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+
+#
+# Beam-it: Bulk clone Github Repositories
+# This bash script will bulk clone Github repositories for a specific user/organisation or team
+#
+# The script will automatically configure itself by asking the user to enter some required information
+# in order to be able to communicate with the Github API
+# When running the script for the first time, the user will be prompted to enter his Github username and API personal access token
+# For more information on API personal access tokens: https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+#
+# Note: After first setup, you might need to source your .bash_profile, .bashrc or .zshrc in order for the variables export to take effect and not to be prompted again
+#
+# Usage: git beam-it <options>
+#
+# Options:
+# -h   		help 		show help
+# -i 		interactive     interactive clone mode. The user will be prompted before cloning each repo
+# -p        	pulbic          clone only public repositories (note that this does not work for teams)
+# -v 		private         clone only private repositories (note that this does not work for teams)
+# -r        	regex           filter repositories based on this regex
+# -t <teamId>    team            clone only repositories belonging to this specific team id
+# -o <orgName>   organisation    clone only repositories belonging to this specific organisation name
+#
+# If the paramteres -t and -o have been left empty, then the script will fetch the list of ogranisations and teams for that specific user
+# The user will then be prompted to enter the organisation name or team id or just skip to fetch all repositories
+#
+# Examples:
+#
+# Clone interactively all the private repositories for the user
+# git beam-it -v -i
+#
+# Clone interactively all the public repos that match the regex .*SeedJobs.* (any repo that contain SeedJobs)
+# git beam-it -i -r .*SeedJobs.*
+#
+# Clone all the public repositores for organisation SeedJobs
+# git beam-it -p -o SeedJobs
+#
+
+# Colors and visual Configurations
+MAGENTA='\033[35m'
+RED='\033[31m'
+YELLOW='\033[33m'
+NC='\033[0m'
+
+die () {
+    echo "ERROR: $*. Aborting." >&2
+    exit 1
+}
+
+check_jq() {
+	if ! type jq &> /dev/null ; then
+	    if [[ "$OSTYPE" == "darwin"* ]]; then
+	        brew 2>/dev/null && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	    fi
+	    printf "The required package ${YELLOW}jq${NC} was not found .. installing now\n"
+	    [[ "$OSTYPE" == "linux-gnu" ]] && sudo apt-get install jq || brew install jq
+	fi
+}
+
+# Set the global environment variables
+set_environment_variable() {
+
+    # Setting $BASH to maintain backwards compatibility
+    # Getting the user's OS type in order to load the correct installation and configuration scripts
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        printf "\nexport ${1}=${2}" >> "${HOME}/.bashrc"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        printf "\nexport ${1}=${2}" >> "${HOME}/.bash_profile"
+    fi
+
+    # Check if we have a .zshrc regardless of the os .. and copy that to the zsh source file
+    if [[ -f "$HOME/.zshrc" ]]; then
+        printf "Noticed that you have Zsh installed .. adding ${1} variable in there .."
+        printf "\nexport ${1}=${2}" >> "${HOME}/.zshrc"
+    fi
+}
+
+# Set the BEAMERY_HOME global environment variable
+if [ -z "$GITHUB_USERNAME" ];
+then
+    printf "We noticed you do not have your ${RED}Github username${NC} variable set ...\n"
+    # Prompt user to select the location for beamery folder
+    read -p "Can you please enter your Github username ? " -e GITHUB_USERNAME
+    set_environment_variable GITHUB_USERNAME $GITHUB_USERNAME
+fi
+
+# Set the BEAMERY_HOME global environment variable
+if [ -z "$GITRIEVAL_TOKEN" ];
+then
+    printf "We noticed you do not have the ${RED}Github Personal Token${NC} variable set ...\nFor more information: ${MAGENTA}https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/${NC}\n"
+    # Prompt user to select the location for beamery folder
+    read -p "Can you please enter your Github personal token so that we can access the Github API ? " -e GITRIEVAL_TOKEN
+    set_environment_variable GITRIEVAL_TOKEN $GITRIEVAL_TOKEN
+fi
+
+
+get_teams() {
+	check_jq
+	printf "We have noticed that you have specified the team ${YELLOW}-t${NC} flag, but have not defined a team\nHere are a list of your teams and their ids in case you did not know them:\n${MAGENTA}"
+	curl --silent https://api.github.com/user/teams?access_token=$GITRIEVAL_TOKEN  | jq '.[] | .id, "name:" + .name, "organisation:" + .organization.login' | sed 'N;N;s/\n/ /g'  | sed 's/\"//g'
+	printf "${YELLOW}Note: you need to provide the team id and not the team name${NC}\n"
+	printf "${NC}Press ${YELLOW}[ENTER]${NC} to skip the team flag and fetch all repositories for the user\n"
+	read -p "Which team id you wish to fetch repositories for: ? " -e TEAM
+}
+
+get_organisations() {
+	check_jq
+	printf "We have noticed that you have specified the organisation ${YELLOW}-o${NC} flag, but have not defined an organisation\nHere are a list of your organisations in case you did not know them:\n${MAGENTA}"
+	curl --silent https://api.github.com/user/orgs?access_token=$GITRIEVAL_TOKEN  | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w login | cut -d "|" -f2
+	printf "${NC}Press ${YELLOW}[ENTER]${NC} to skip the organisation flag and fetch all repositories for the user\n"
+	read -p "Which organisation you wish to fetch repositories for: ? " -e ORGANISATION
+}
+
+# Initialize our own variables:
+OPTIND=1; ORGANISATION=false; TEAM=false; REGEX=false; IS_PRIVATE=false; IS_PUBLIC=false; IS_INTERACTIVE=false; VISIBILITY='all'
+# Parse the options and arguments passed to the function
+while getopts ":o:t:r:pvih" opt; do
+    case "$opt" in
+    h) 	grep '#' "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" | cut -c 2- | head -n +37 | tail -n +2 && exit 0
+		;;
+    o)  if [[ ${OPTARG:0:1} == '-' ]]; then
+            die "Invalid value $OPTARG given to -$OPTION"
+        fi
+		ORGANISATION=$OPTARG
+        ;;
+    t)  if [[ ${OPTARG:0:1} == '-' ]]; then
+            die "Invalid value $OPTARG given to -$OPTION"
+        fi
+		TEAM=$OPTARG
+        ;;
+	r)  if [[ ${OPTARG:0:1} == '-' ]]; then
+            die "Invalid value $OPTARG given to -$OPTION"
+        fi
+		REGEX=$OPTARG
+        ;;
+    i)  IS_INTERACTIVE=true
+		;;
+    v)  $IS_PUBLIC && die "Cannot retrieve both private and public repos with arguments (FYI: default behaivour is all)"
+		IS_PRIVATE=true
+		VISIBILITY="private"
+        ;;
+    p)  $IS_PRIVATE && die "Cannot retrieve both private and public repos with arguments (FYI: default behaivour is all)"
+		IS_PUBLIC=true
+		VISIBILITY="public"
+		;;
+	\?) die "Invalid option passed" ;;
+	:)  if [ $OPTARG == "t" ]; then
+			get_teams
+		elif [ $OPTARG == "o" ]; then
+			get_organisations
+		else
+			echo "$0: -$OPTARG needs a value" >&2; exit 2
+		fi
+		;;
+
+    esac
+done
+
+shift $((OPTIND-1))
+[ "$1" = "--" ] && shift
+
+# Given a list of repos URLs .. clone or pull and update the list
+get_repos() {
+	echo "Beaming down all the repositories now ..."
+	for repository in $@
+	do
+		REPOSITORY_NAME=`echo $(basename $repository) | cut -d"." -f1`
+		# Check if the URI matches a pattern if defined
+		if [[ $REGEX != false ]]; then
+			! [[ $repository =~ $REGEX ]] && printf "\nSkipping ${RED}$REPOSITORY_NAME${NC} as it does not match the regex filter" && continue
+		fi
+		if [[ $IS_INTERACTIVE == true ]]; then
+			echo ""
+	    	printf "Do you need to clone repository: ${MAGENTA}${REPOSITORY_NAME}${NC}? [Y/N] " && read -n1;
+	    	if [[ $REPLY =~ ^[yY]$ ]]; then
+	    		echo ""
+	    		[ -d "$REPOSITORY_NAME" ] && printf "\n${RED}Repository already exists .. pulling new changes${NC}\n" && git -C "$REPOSITORY_NAME" pull origin master || git clone "http://$(echo $repository|sed -e 's/[\r\n]//g')"
+	    	fi
+	    else
+	    	[ -d "$REPOSITORY_NAME" ] && printf "\n${RED}Repository already exists .. pulling new changes${NC}\n" && git -C "$REPOSITORY_NAME" pull origin master || git clone "http://$(echo $repository|sed -e 's/[\r\n]//g')"
+    	fi
+	done
+}
+
+# Iteratively request data from an API endpoint
+request_github_api() {
+	GITHUB_PAGE=1
+	until [[ $IS_PAGINATED == false ]]; do
+		REPOS=$(curl --silent -i -H "Authorization: token ${GITRIEVAL_TOKEN}" "https://api.github.com/${1}&per_page=100&page=${GITHUB_PAGE}" | awk -v RS=',"' -F: '/^clone_url/ {print $3}' | sed 's/["]//g' | cut -c 3-)
+		REPOLIST+="\n${REPOS}"
+		printf "Retrieving batch: ${GITHUB_PAGE} of repositores --> ${YELLOW} `echo "$REPOLIST" | wc -l` ${NC} processed so far\n"
+		if [[ -n $REPOS ]]; then
+    		(( GITHUB_PAGE++ ))
+		else
+		    printf "Finished retreiving allrepositores with Total: ${YELLOW} `echo "$REPOLIST" | wc -l` ${NC}\n"
+		    IS_PAGINATED=false
+		fi
+	done
+	get_repos $REPOLIST
+}
+
+if [[ $ORGANISATION != false && -n $ORGANISATION ]]; then
+	printf "Retreiving ${RED}${VISIBILITY}${NC} repositories for organisation: ${ORGANISATION}\n"
+	request_github_api "orgs/$ORGANISATION/repos?type=${VISIBILITY}"
+elif [[ $TEAM != false && -n $TEAM ]]; then
+	printf "Retreiving ${RED}${VISIBILITY}${NC} repositories for team: ${TEAM}\n"
+	request_github_api "teams/$TEAM/repos?visibility=${VISIBILITY}"
+else
+	printf "Since no team or organisation were defined, retreiving ${RED}${VISIBILITY}${NC} repositories for the user\n"
+	request_github_api "user/repos?visibility=${VISIBILITY}"
+fi
